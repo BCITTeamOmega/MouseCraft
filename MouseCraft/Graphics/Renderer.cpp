9c0b1d6ea99c9081283613d0b5c3dd5c18d75e29
Aaron Freytag <aaroncfreytag@gmail.com>
2019-02-08 00:28:19 -0800

    Made things a little nicer

--- MouseCraft/Graphics/Renderer.cpp
+++ MouseCraft/Graphics/Renderer.cpp
@@ -1,18 +1,18 @@
 #include "Renderer.h"
+#include <string>
+
+using std::string;
 
 void Renderer::initialize() {
-	static int width = 640;
-	static int height = 480;
-	static int startx = SDL_WINDOWPOS_UNDEFINED;
-	static int starty = SDL_WINDOWPOS_UNDEFINED;
+	static const int width = 640;
+	static const int height = 480;
+	static const int x = SDL_WINDOWPOS_UNDEFINED;
+	static const int y = SDL_WINDOWPOS_UNDEFINED;
+
+	static const char* title = "Mousecraft";
 
 	SDL_Init(SDL_INIT_VIDEO);
-	window = SDL_CreateWindow("Mousecraft", 
-		startx, 
-		starty, 
-		width,
-		height,
-		SDL_WINDOW_OPENGL);
+	window = SDL_CreateWindow(title, x, y, width, height, SDL_WINDOW_OPENGL);
 	//Get window surface
 	surface = SDL_GetWindowSurface(window);
 }
@@ -21,6 +21,7 @@ void Renderer::queueRender(const RenderData& m) {
 }
 
 void Renderer::render() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	SDL_UpdateWindowSurface(window);
 }
 
