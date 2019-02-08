03b4542522205497aed60c8dc44534381bd8fc82
Aaron Freytag <aaroncfreytag@gmail.com>
2019-02-07 21:58:57 -0800

    Included SDL

--- MouseCraft/Graphics/Renderer.cpp
+++ MouseCraft/Graphics/Renderer.cpp
@@ -1,1 +1,8 @@
 #include "Renderer.h"
+#include <sdl/SDL.h>
+
+void Renderer::initialize() {
+	SDL_Window *window;
+	SDL_Init(SDL_INIT_VIDEO);
+	window = SDL_CreateWindow("Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
+}
