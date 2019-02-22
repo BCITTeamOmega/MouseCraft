7ef87f21292af4ffed25a5026c838efe5d9d18d7
Jason Cheung <jayson.h.cheung@gmail.com>
2019-02-21 19:46:51 -0800

    WIP: Deadzone handling

--- MouseCraft/Input/InputSystem.h
+++ MouseCraft/Input/InputSystem.h
@@ -62,6 +62,152 @@ struct AnalogEvent
 
 class InputSystem : public System
 {
+	// class that helps parse 2d input for SDL.
+	// Optionally call SetX() and/or SetY()
+	// Always call Update() once per frame, after all input has been read. 
+	// Use Has...Changed()
+	class Axis2DInput
+	{
+	public:
+		// shared 
+		float deadzone = 0.1f;
+		float rawX;					// raw current value
+		float rawY;					// raw current value
+		// individual 
+		float lastX;
+		float lastY;
+		bool xChanged = false;
+		bool yChanged = false;
+		// axis
+		glm::vec2 lastAxis;
+		bool axisChanged = false;
+
+
+		void SetX(float x)
+		{
+			rawX = x;
+		};
+
+		void SetY(float y)
+		{
+			rawY = y;
+		};
+
+		void Update()
+		{
+			xChanged = false;
+			yChanged = false;
+			axisChanged = false;
+
+			// parse indiv - x
+			if (lastX == 0)
+			{
+				// dead 
+				if (abs(rawX) > deadzone)
+				{
+					xChanged = true;
+					lastX = rawX;
+				}
+				// else ignore 
+			}
+			else
+			{
+				// active 
+				if (abs(rawX) < deadzone)
+				{
+					lastX = 0;
+				}
+				else
+				{
+					lastX = rawX;
+				}
+				xChanged = true;
+			}
+
+			// parase indiv - y
+			if (lastY == 0)
+			{
+				// dead 
+				if (abs(rawY) > deadzone)
+				{
+					yChanged = true;
+					lastY = rawY;
+				}
+				// else ignore 
+			}
+			else
+			{
+				// active 
+				if (abs(rawY) < deadzone)
+				{
+					lastY = 0;
+				}
+				else
+				{
+					lastY = rawY;
+				}
+				yChanged = true;
+			}
+
+			// parse axis 
+			// note: possible for x and y not to change but for axis to change. (x=0.9, y=0.9, axis>1)
+			auto length = sqrt(rawX*rawX + rawY * rawY);
+			if (lastAxis == glm::vec2())
+			{
+				// dead 
+				if (length > deadzone)
+				{
+					axisChanged = true;
+					lastAxis = glm::vec2(rawX, rawY);
+				}
+			}
+			else
+			{
+				// active 
+				if (length < deadzone)
+				{
+					lastAxis = glm::vec2();
+				}
+				else
+				{
+					lastAxis = glm::vec2(rawX, rawY);
+				}
+				axisChanged = true;
+			}
+		}
+
+		bool HasXChanged()
+		{
+			return xChanged;
+		}
+
+		bool HasYChanged()
+		{
+			return yChanged;
+		}
+
+		bool HasAxisChanged()
+		{
+			return axisChanged;
+		}
+
+		float GetX()
+		{
+			return lastX;
+		}
+
+		float GetY()
+		{
+			return lastY;
+		}
+
+		glm::vec2 GetAxis()
+		{
+			return lastAxis;
+		}
+	};
+
+
 public:
 	InputSystem()
 	{
