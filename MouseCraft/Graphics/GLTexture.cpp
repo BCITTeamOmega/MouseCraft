96505b074b0bcf5b3fc672b4b43aae07bd068f51
Aaron Freytag <aaroncfreytag@gmail.com>
2019-02-25 01:09:19 -0800

    Added classes for OpenGL textures

---
+++ MouseCraft/Graphics/GLTexture.cpp
@@ -0,0 +1,41 @@
+#include "GLTexture.h"
+GLTexture::GLTexture() {
+	glGenTextures(1, &_id);
+}
+
+GLTexture::~GLTexture() {
+	glDeleteTextures(1, &_id);
+}
+
+void GLTexture::setImage(Image image, bool mipmap) {
+	glBindTexture(GL_TEXTURE_2D, _id);
+	GLuint format = GL_RGBA;
+	glTexImage2D(
+		_id,
+		0,
+		format,
+		image.getWidth(),
+		image.getHeight(),
+		0,
+		format,
+		GL_UNSIGNED_BYTE,
+		image.getData()
+	);
+	if (mipmap) {
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+}
+
+void GLTexture::bind(GLenum slot) {
+	glActiveTexture(slot);
+	glBindTexture(GL_TEXTURE_2D, _id);
+}
+
+void GLTexture::unbind(GLenum slot) {
+	glActiveTexture(slot);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+GLuint GLTexture::getID() {
+	return _id;
+}
---
+++ MouseCraft/Graphics/GLTexture.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "../GL/glad.h"
+#include "Image.h"
+class GLTexture {
+public:
+	GLTexture();
+	~GLTexture();
+	GLuint getID();
+	void setImage(Image image, bool mipmap = true);
+	void bind(GLenum slot);
+	void unbind(GLenum slot);
+private:
+	GLuint _id;
+};
