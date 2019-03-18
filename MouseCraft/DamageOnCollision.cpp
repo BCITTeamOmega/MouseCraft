2de38befd620942ed1a399747f4f762dbe65c78c
Jason Cheung <jayson.h.cheung@gmail.com>
2019-03-18 16:08:22 -0700

    Additions to damage on collision

--- MouseCraft/DamageOnCollision.cpp
+++ MouseCraft/DamageOnCollision.cpp
@@ -2,6 +2,11 @@
 
 #include "HealthComponent.h"
 
+DamageOnCollision::DamageOnCollision() : 
+	_handleOnCollision(this, &DamageOnCollision::OnCollision)
+{
+}
+
 DamageOnCollision::DamageOnCollision(std::set<PhysObjectType::PhysObjectType> checkFor) :
 	_handleOnCollision(this, &DamageOnCollision::OnCollision),
 	_checkFor(checkFor)
@@ -17,15 +22,23 @@ void DamageOnCollision::OnInitialized()
 	GetEntity()->GetComponent<PhysicsComponent>()->onCollide.Attach(_handleOnCollision);
 }
 
+void DamageOnCollision::SetLayers(std::set<PhysObjectType::PhysObjectType> checkFor)
+{
+	_checkFor = checkFor;
+}
+
 void DamageOnCollision::OnCollision(PhysicsComponent * other)
 {
 	if (_checkFor.find(other->type) != _checkFor.end())
 	{
-		auto health = other->GetEntity()->GetComponent<HealthComponent>();
-		if (health)
-			health->Damage(1);
-		else
-			std::cout << "WARNING: DamageOnCollision couldn't find health to damage." << std::endl;
+		if (damage > 0)
+		{
+			auto health = other->GetEntity()->GetComponent<HealthComponent>();
+			if (health)
+				health->Damage(1);
+			else
+				std::cout << "WARNING: DamageOnCollision couldn't find health to damage." << std::endl;
+		}
 
 		if (destroyOnCollision)
 			GetEntity()->Destroy();
