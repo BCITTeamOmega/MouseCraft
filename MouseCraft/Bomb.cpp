d681e9f56b391032dcdda1bd33bbd36113f14ce6
Jason Cheung <jayson.h.cheung@gmail.com>
2019-03-18 16:09:05 -0700

    Implemented bomb

--- MouseCraft/Bomb.cpp
+++ MouseCraft/Bomb.cpp
@@ -2,6 +2,12 @@
 
 #include "Mice.h"
 
+const float Bomb::RADIUS	= 10.0f;
+const float Bomb::LIFETIME	= 2.0f;
+const float Bomb::SPEED		= 12.5f;
+const int Bomb::DAMAGE		= 2;
+
+
 Bomb::Bomb()
 {
 }
@@ -9,42 +15,66 @@ Bomb::Bomb()
 
 Bomb::~Bomb()
 {
+	Explode();
+}
+
+void Bomb::OnInitialized()
+{
+	Contraption::OnInitialized();
+	_physics = GetEntity()->GetComponent<PhysicsComponent>();
+	_timed = GetEntity()->GetComponent<TimedDestruction>();
+	_dcollision = GetEntity()->GetComponent<DamageOnCollision>();
 }
 
 bool Bomb::use() {
 	Contraption::use();
 
-	// need to dissapear after exploding
-	fieldEntity->SetParent(OmegaEngine::Instance().GetRoot());
-	fieldEntity->transform.setLocalPosition(GetEntity()->transform.getWorldPosition());
-	fieldEntity->SetEnabled(true);
- 
 	std::cout << "BOMB is being used" << std::endl;
-	PhysicsComponent* pc = GetEntity()->GetComponent<PhysicsComponent>();
-	std::vector<PhysObjectType::PhysObjectType> stuff;
 
-	if (pc->type = PhysObjectType::CONTRAPTION_DOWN) {
-		stuff.push_back(PhysObjectType::CAT_DOWN);
-		stuff.push_back(PhysObjectType::OBSTACLE_DOWN);
-	}
+	// launching position
+	auto dir = GetEntity()->t().wForward();
+	auto pos = GetEntity()->t().wPos() + dir * 5.0f;
 
-	if (pc->type = PhysObjectType::CONTRAPTION_UP) {
-		stuff.push_back(PhysObjectType::CAT_UP);
-		stuff.push_back(PhysObjectType::OBSTACLE_UP);
-	}
+	// need to dissapear after exploding
+	GetEntity()->SetParent(OmegaEngine::Instance().GetRoot());
+	GetEntity()->transform.setLocalPosition(pos);	// remember physics will override this 
 
-	auto p1 = fieldEntity->transform;
-	auto pos = p1.getWorldPosition();
-	auto bl = pos - glm::vec3(-1, 0, -1);
-	auto tr = pos - glm::vec3(1, 0, 1);
-	//if (pc->areaCheck(stuff, new Vector2D(bl.x, bl.z), new Vector2D(tr.x, tr.z), true)) {
-	//	this->Drop();
-	//	this->GetEntity()->Destroy();
-	//}
+	// make contraption "active"
+	_physics->SetEnabled(true);
+	_timed->SetEnabled(true);
+	_dcollision->SetEnabled(true);
+	
+	// configure final settings 
+	_checkFor =  
+	{
+		PhysObjectType::CONTRAPTION_DOWN,
+		PhysObjectType::CAT_DOWN,
+		PhysObjectType::WALL,
+		PhysObjectType::CONTRAPTION_UP,
+		PhysObjectType::CAT_UP
+	};
+	_dcollision->SetLayers(_checkFor);
+	_physics->moveBody(new Vector2D(pos.x, pos.z), 0);
+	auto vel = glm::vec2(dir.x, dir.z) * SPEED;
+	_physics->velocity = Vector2D(vel);
 
 	return true;
 }
 
 void Bomb::show() {
 	Contraption::use();
+}
+
+void Bomb::Explode()
+{
+	auto pos = GetEntity()->t().wPos();
+	auto bl = pos + glm::vec3(-1, 0, -1) * RADIUS;
+	auto tr = pos + glm::vec3(1, 0, 1) * RADIUS;
+	auto hits = PhysicsManager::instance()->areaCheck(nullptr, _checkFor, new Vector2D(bl.x, bl.z), new Vector2D(tr.x, tr.z));
+
+	for (auto p : hits)
+	{
+		auto health = p->GetEntity()->GetComponent<HealthComponent>();
+		if (health) health->Damage(DAMAGE);
+	}
 }
